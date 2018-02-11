04b17f381bb062b1d635b9e028f605070130d3b7
Andrew Chen <andrewchen@Andrews-MacBook-Pro.local>
2018-02-10 19:54:48 -0500

    smol change

--- client/src/components/Games/TypeFaster/index.js
+++ client/src/components/Games/TypeFaster/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const TypeFaster = () => {
-
+  return (
+    <div className='type-faster'>
+      <h1>TypeFaster</h1>
+    </div>
+  )
 }
 
 export default TypeFaster
--- client/src/components/Games/index.js
+++ client/src/components/Games/index.js
@@ -1,17 +1,22 @@
-import React, {Component} from 'react'
-import SpaceSpam from './SpamSpacebar'
-import TypeFaster from './TypeFaster'
+import React, { Component } from "react";
+import SpaceSpam from "./SpamSpacebar";
+import TypeFaster from "./TypeFaster";
 
 export default class Game extends Component {
-    constructor(props) {
-      super(props)
-      this.state ={}
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameId: 0,
+      games: [<SpaceSpam />, <TypeFaster />]
+    };
+  }
 
-    render() {
-      return (
-        <div>
-        </div>
-      )
-    }
+  componentDidMount() {
+    this.setState(
+      { gameId: Math.floor(Math.random() * this.state.games.length) });
+  }
+
+  render() {
+    return <div>{this.state.games[this.state.gameId]}</div>;
+  }
 }
